79d00b0ce510a0ea8bea90266183c732bc9d10df
jppetr <jppertr@gmail.com>
2025-10-21 16:15:14 -0400

    Randomizer and some animation

--- app/src/main/java/com/zybooks/roulette/MainActivity.java
+++ app/src/main/java/com/zybooks/roulette/MainActivity.java
@@ -7,8 +7,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import java.util.Random;
+import android.animation.Animator;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int[] Roul_Num = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+            15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36
+
+    };
+
+    public int wheelspin() {
+        Random random = new Random();
+        int RandIndex = random.nextInt(Roul_Num.length);
+        return Roul_Num[RandIndex];
+    };
+
+    public void spinWheel(ImageView roulette_wheel){
+        //number of spins on the wheel
+        int spinDegrees = 360 * 3 + new Random().nextInt();
+        animator.setDuration(2000);
+        animator.setInterpolator(next.Int)
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,4 +40,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+
 }

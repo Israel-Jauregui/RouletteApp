8d6879c49b2aa4a14f6cf7445272249ddf15656f
tory-cmd <toryt1912@gmail.com>
2025-10-23 16:22:05 -0400

    cdf

--- app/src/main/java/com/zybooks/roulette/MainActivity.java
+++ app/src/main/java/com/zybooks/roulette/MainActivity.java
@@ -1,39 +1,21 @@
 package com.zybooks.roulette;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import java.util.Random;
-import android.animation.Animator;
-import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
-
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int[] Roul_Num = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
-            15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36
-
-    };
-
-    public int wheelspin() {
-        Random random = new Random();
-        int RandIndex = random.nextInt(Roul_Num.length);
-        return Roul_Num[RandIndex];
-    };
-
+    private RouletteWheel rouletteWheel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Test1
-        //test2
 
+        ImageView wheelView = findViewById(R.id.rouletteTable);
+
+        // Attach the fling/spin behavior
+        rouletteWheel = new RouletteWheel(wheelView);
     }
-
 }

b0b03be67beffcd7ea599df5eb2b0194474f0191
Israel-Jauregui <ijauregui222@gmail.com>
2025-10-30 16:29:00 -0400

    Commit

---
+++ app/src/main/java/com/zybooks/roulette/User.java
@@ -0,0 +1,34 @@
+package com.zybooks.roulette;
+import android.content.SharedPreferences;
+import android.content.Context;
+public class User {
+
+
+
+    SharedPreferences currencyTracker;
+    SharedPreferences.Editor editor;
+
+    public User(Context context)
+    {
+    currencyTracker = context.getSharedPreferences("currencyVal",Context.MODE_PRIVATE);
+    editor = currencyTracker.edit();
+    }
+
+
+    public int getMoney()
+    {
+
+
+        return currencyTracker.getInt("Money", 500);
+    }
+
+    public void setMoney(int moneyParam)
+    {
+       editor.putInt("Money", moneyParam);
+        editor.apply();
+    }
+
+
+
+
+}
